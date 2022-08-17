04c219d20f7f7e0c24592156cc58f34203c355f8
asafMaza <observur@gmail.com>
2022-08-18 00:26:53 +0300

    end of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -36,10 +36,14 @@ namespace API
             });
 
             services.AddControllers();
+           services.AddCors();
+            
+            #region 
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,6 +59,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
